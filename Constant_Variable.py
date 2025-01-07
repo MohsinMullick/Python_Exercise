21c8bcb89890911d8e1a1c6870a545ed20d2b00f
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-01-08 04:45:01 +0600

    Add files via upload

---
+++ Constant_Variable.py
@@ -0,0 +1,7 @@
+"""Constants and Variables:
+Define a constant for the value of pi (3.14159).
+Then use it to calculate the area of a circle, where the radius is stored in a variable."""
+pi=3.14159
+radius=3
+area=pi*radius*radius
+print(f"area of a circle:{area}")
