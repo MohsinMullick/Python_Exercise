d69e576d36d724cb1b2b464cdd46a51d6176742d
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-01-08 04:22:27 +0600

    Add files via upload

---
+++ Temperature_Conversion.py
@@ -0,0 +1,6 @@
+"""Temperature Conversion:
+Create a program that converts a temperature from Celsius to Fahrenheit.
+ Use a formula where Fahrenheit = (Celsius * 9/5) + 32."""
+Celsius=18
+Fahrenheit=(Celsius*9/5)+32
+print(f"Celsius to Fahrenheit:{Fahrenheit}")
