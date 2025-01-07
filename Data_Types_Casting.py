492d860b17018ad988c0ec03242681bc028a0fbd
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-01-08 05:09:56 +0600

    Add files via upload

---
+++ Data_Types_Casting.py
@@ -0,0 +1,13 @@
+"""Data Types and Casting:
+ Create three variables:An integer	A float
+	A string
+Print the data type of each variable, then cast them all to strings and print the result."""
+
+
+
+x=20
+y="Im Mohsin"
+z=40.64
+print(f"data types: {type(x)}")
+print(f"data types: {type(y)}")
+print(f"data types: {type(z)}")
