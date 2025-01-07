5768055eaaff2a2a16271d86788cbd45d38d6454
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-01-08 04:38:00 +0600

    Add files via upload

---
+++ Simple_Input_Output.py
@@ -0,0 +1,13 @@
+"""Simple Input/Output:
+Write a program that asks the user for their name and age, and then prints:
+"Hello [name], you are [age] years old!"""
+
+
+print("whats your name?")
+name=input()
+print(f"Hello {name},")
+print("whats your age?")
+age=input()
+print(f"i have {age} years old!")
+
+
