fba0e6b1bde1357ecbe0ef12eb09d6e0335375fc
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-01-10 02:09:14 +0600

    Add files via upload

---
+++ Create_List_Of_Fruits.py
@@ -0,0 +1,9 @@
+"""Create a List of Fruits:
+Create a list of five different fruits. Print each fruit one at a time using a for loop.
+"""
+fruits=['apple','orange','banana','strawberry','mango']#declears list variable and values
+
+for fruit in fruits:#one at a time using a for loop
+    print(fruit)#print function
+
+
