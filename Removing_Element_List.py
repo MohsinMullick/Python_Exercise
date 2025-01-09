8fed18a35d8cc391e021f9b9a5710cf7dd875167
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-01-10 02:35:46 +0600

    Add files via upload

---
+++ Removing_Element_List.py
@@ -0,0 +1,7 @@
+"""for example,
+here’s how to remove the second
+item, 'yamaha', from the list:"""
+motorcycles=['ducati','yamaha','suzuki']
+print(motorcycles)
+del motorcycles[1]
+print(motorcycles)
