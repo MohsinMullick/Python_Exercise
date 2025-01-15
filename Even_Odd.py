23e488dc26c4170072b1e3731cb8fd34a849e102
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-01-15 16:32:12 +0600

    Add files via upload

---
+++ Even_Odd.py
@@ -0,0 +1,6 @@
+"""Write a program that checks if a number is even or odd."""
+numbers=float(input(f"Enter the number:"))
+if numbers%2==0:
+    print(f"The number is even.")
+elif numbers%2!=0:
+    print(f"The number is odd.")
