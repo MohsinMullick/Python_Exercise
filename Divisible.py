5e892e63a922a678d46ccdeb737e98edb0b8f08b
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-01-15 16:43:38 +0600

    Add files via upload

---
+++ Divisible.py
@@ -0,0 +1,6 @@
+"""2.How do you check if a number is divisible by 5 using an if statement?"""
+numbers=float(input(f"Enter the number:"))
+if numbers%5==0:
+    print(f"{numbers} is divisible by 5.")
+else:
+    print(f"{numbers} is not divisible by 5.")
