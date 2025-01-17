18b0601b6c161638da995380f24d630ba6a79f5d
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-01-18 01:36:23 +0600

    Add files via upload

---
+++ Modifying_Elements.py
@@ -0,0 +1,33 @@
+""".Create a list of colors and
+change the first color to a different one.
+ Print the updated list."""
+colors=['green','yellow','black','pink','red']
+print(colors)
+colors[0]="purple"
+print(colors)
+
+""".Make a list of your favorite songs,
+ then update the second and fourth songs.
+  Print the modified list."""
+songs=['unstoppable','bad habits','co2','closer','sick boy']
+print(songs)
+songs[1]="paris"
+songs[3]="jupiter"
+print(songs)
+
+
+"""Create a list of four seasons and
+ replace the last season with a holiday name. Print the list."""
+seasons=['spring','summer','winter','autumn']
+seasons[-1]="fall"
+print(f"holiday:{seasons}")
+
+
+""".Store four different programming languages in a list and 
+change the language at index 2 to your favorite one.
+ Print the updated list."""
+programming_language_name=['c','java','c#','java script']
+programming_language_name[2]="python"
+print(programming_language_name)
+
+
