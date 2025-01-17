dc61e0e54a8abb22db827c31ea3a7606b2675c37
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-01-18 02:04:25 +0600

    Add files via upload

---
+++ Adding_Elements.py
@@ -0,0 +1,33 @@
+"""13.Start with an empty list of hobbies and
+ append three hobbies one by one. Print the final list."""
+hobby=[]
+hobby.append("swimming")
+hobby.append("read book")
+hobby.append("games")
+print(hobby)
+
+"""14.	Create a list of your favorite books,
+ then append a new book to the list. 
+ Print the updated list."""
+books_name=['to kill a mockingbird','1948','the great gatsby','pride and prejudice','the catcher in the rye']
+books_name.append("atomic habits")
+print(books_name)
+
+"""15.	Make a list of animals and 
+use the insert() method to add a new animal at index 2.
+ Print the modified list."""
+
+animals=['tiger','horse','elephant']
+animals.insert(2,"lion")
+print(animals)
+
+
+"""16.	Create a list of fruits and
+ add two new fruits at the beginning using insert(). 
+ Print the list."""
+fruits=['banana']
+fruits.insert(0,"apple")
+fruits.insert(1,"orange")
+print(fruits)
+
+
