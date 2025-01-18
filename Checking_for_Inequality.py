7fabba86d7a11597a2a9f3cb0b8f7a67c606ce31
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2025-01-18 07:35:54 +0600

    Add files via upload

---
+++ Checking_for_Inequality.py
@@ -0,0 +1,27 @@
+"""12.Write an if statement to print a message
+ when a variable city is not equal to 'Paris'."""
+message="Paris"
+if message=="Paris":
+    print("The city is equal(Paris)")
+else:
+    print("the city is not equal(Paris)")
+
+"""13.Create a program that uses the != operator to check if a given number is not equal to 0."""
+numbers=10
+if numbers!=0:
+    print("true")
+else:
+    print("false")
+
+"""14.Use the != operator to determine if two lists are different."""
+
+list1 = [1, 2, 3, 4]
+list2 = [1, 2, 3, 5]
+if list1 != list2:
+    print("The lists are different.")
+else:
+    print("The lists are the same.")
+
+
+
+
